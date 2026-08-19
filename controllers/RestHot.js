181b6834b06ac7b41facad20aff6b49dcd085ad2
Sumit Kashyap <sumitkashyapmbd96@gmail.com>
2026-08-19 22:19:30 +0530

    Add signed URLs for RestHot images

--- controllers/RestHot.js
+++ controllers/RestHot.js
@@ -1,9 +1,10 @@
 const FoodMenu = require("../models/Restaurants/AddMenu");
-const Hotel = require('../models/Hotels/HotelModel');
-const AddRestaurent = require('../models/Restaurants/AddRestaurent');
+const Hotel = require("../models/Hotels/HotelModel");
+const AddRestaurent = require("../models/Restaurants/AddRestaurent");
+
+const { getSignedImageUrl } = require("./services/s3Service");
 
 const getRestHot = async (req, res) => {
-
     try {
 
         const { city } = req.params;
@@ -19,37 +20,119 @@ const getRestHot = async (req, res) => {
             restaurant: { $in: restaurantIds }
         });
 
-        // attach menu to each restaurant
+        // =========================================
+        // HOTEL SIGNED URLS
+        // =========================================
 
-        const restaurantWithMenu = restaurants.map( r => {
-            const menu = foodMenus.filter(
-                f => f.restaurant.toString() === r._id.toString()
-            )
+        const hotelsWithSignedUrls = await Promise.all(
+            hotels.map(async (hotel) => {
 
-            return {
-                ...r.toObject(),
-                menu 
-            }
-        })
+                const hotelData = hotel.toObject();
 
+                if (hotelData.hotelImages?.length) {
+
+                    hotelData.hotelImages = await Promise.all(
+                        hotelData.hotelImages.map(async (image) => {
+
+                            if (image.key) {
+                                image.url = await getSignedImageUrl(image.key);
+                            }
+
+                            return image;
+                        })
+                    );
+                }
+
+                return hotelData;
+            })
+        );
+
+        // =========================================
+        // RESTAURANT + FOOD SIGNED URLS
+        // =========================================
+
+        const restaurantWithMenu = await Promise.all(
+
+            restaurants.map(async (restaurant) => {
+
+                const restaurantData = restaurant.toObject();
+
+                // Restaurant images
+                if (restaurantData.images?.length) {
+
+                    restaurantData.images = await Promise.all(
+                        restaurantData.images.map(async (image) => {
+
+                            if (image.key) {
+                                image.url = await getSignedImageUrl(image.key);
+                            }
+
+                            return image;
+                        })
+                    );
+                }
+
+                // Restaurant menu
+                const menu = foodMenus.filter(
+                    food =>
+                        food.restaurant.toString() ===
+                        restaurant._id.toString()
+                );
+
+                // Food images
+                const signedMenu = await Promise.all(
+
+                    menu.map(async (food) => {
+
+                        const foodData = food.toObject();
+
+                        if (foodData.foodImages?.length) {
+
+                            foodData.foodImages = await Promise.all(
+                                foodData.foodImages.map(async (image) => {
+
+                                    if (image.key) {
+                                        image.url =
+                                            await getSignedImageUrl(image.key);
+                                    }
+
+                                    return image;
+                                })
+                            );
+                        }
+
+                        return foodData;
+                    })
+                );
+
+                return {
+                    ...restaurantData,
+                    menu: signedMenu
+                };
+            })
+        );
+
+        // =========================================
+        // RESPONSE
+        // =========================================
 
         res.json({
-
             success: true,
             data: {
-                hotels,
+                hotels: hotelsWithSignedUrls,
                 restaurants: restaurantWithMenu
             }
-        })
+        });
 
     } catch (err) {
-        console.log("Error:", err)
+
+        console.log("Error:", err);
+
         res.status(500).json({
             success: false,
             message: "Server Error"
-        })
-
+        });
     }
-}
+};
 
-module.exports = getRestHot
+module.exports = getRestHot;
